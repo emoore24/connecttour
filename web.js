2ed739fd4f3fd1ef12d892557888c29545fe389d
emoore24 <evan.moore24@gmail.com>
2013-05-02 16:19:32 -0400

    adding connection string

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+var connString = "postgres://shcpmwtwyxuxax:IFYCad_h0oQi_YAvjercNOIsto@ec2-54-235-152-226.compute-1.amazonaws.com:5432/dfu6b4s2s6n3v1";
 app.use(express.logger());
 
 app.get('/', function(request, response) {
