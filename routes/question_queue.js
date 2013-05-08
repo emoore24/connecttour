7788f189702371c77026859c22a3f43e55e9aaa6
Tara Krishnan <tara.krish16@gmail.com>
2013-05-08 15:37:05 -0400

    question_queue implemented in real time with socket.io but not tested

--- routes/question_queue.js
+++ routes/question_queue.js
@@ -3,6 +3,7 @@
  */
 
 exports.show = function(req, res){
+    module.io = module.parent.io;
 	var template_engine = req.app.settings.template_engine;
 	res.locals.session = req.session;
     res.render('question_queue');
