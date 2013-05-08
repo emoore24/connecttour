3c02d1369d891abeb2554ca53210ed84a2e53a82
emoore24 <evan.moore24@gmail.com>
2013-05-08 11:54:44 -0400

    fixing main, adding ejs to stats

--- routes/main.js
+++ routes/main.js
@@ -1,65 +1,69 @@
 
 
-if (checkin) {
-    pg.connect(pgconnstring, function (err, client, done) {
-        if (err) {
-            // error!
-            done();
-        } else {
-
-        	var time_query = tour
-        		.select(tour.start_time, tour.end_time)
-        		.from(tour)
-        		.where(
-        			tour.tour_id.equals(current_tour)
-        		).toQuery();
-
-            console.log(time_query);
-
-        	var guide_query = user
-                .select(user.first_name, user.last_name, user.profile_pic)
-                .from(user)
-                .where(
-                    user.user_id.equals(current_guide)
-                ).toQuery();
-
-            client.query(time_query, [], function (err, result) {
-                if (err) {
-                    // error!
-                } else if (result.rows.length < 1) {
-                    // no user found with that username!
-                } else {
-                    console.log(result.rows[0].start_time);
-                    start_time = result.rows[0].start_time;
-                    end_time = result.rows[0].end_time;
-                }
-                done();
-            });
+console.log("MAIN");
 
 
-            client.query(guide_query, [], function (err, result) {
-                if (err) {
-                    // error!
-                } else if (result.rows.length < 1) {
-                    // no user found with that username!
-                } else {
-                    console.log(result.rows[0].profile_pic);
-                    first_name = result.rows[0].first_name;
-                    last_name = result.rows[0].last_name;
-                    pic_url = result.rows[0].profile_pic;
-                }
-                done();
-            });
-        }
-    })
-}
+
 
 
 exports.show = function(req, res){
+    if (tour_checkin) {
+        pg.connect(pgconnstring, function (err, client, done) {
+            if (err) {
+                // error!
+                done();
+            } else {
+
+                var time_query = tour
+                    .select(tour.start_time, tour.end_time)
+                    .from(tour)
+                    .where(
+                        tour.tour_id.equals(current_tour)
+                    ).toQuery();
+
+                console.log(time_query);
+
+                var guide_query = user
+                    .select(user.first_name, user.last_name, user.profile_pic)
+                    .from(user)
+                    .where(
+                        user.user_id.equals(current_guide)
+                    ).toQuery();
+
+                client.query(time_query, [], function (err, result) {
+                    if (err) {
+                        // error!
+                    } else if (result.rows.length < 1) {
+                        // no user found with that username!
+                    } else {
+                        console.log(result.rows[0].start_time);
+                        start_time = result.rows[0].start_time;
+                        end_time = result.rows[0].end_time;
+                    }
+                    done();
+                });
+
+
+                client.query(guide_query, [], function (err, result) {
+                    if (err) {
+                        // error!
+                    } else if (result.rows.length < 1) {
+                        // no user found with that username!
+                    } else {
+                        console.log(result.rows[0].profile_pic);
+                        first_name = result.rows[0].first_name;
+                        last_name = result.rows[0].last_name;
+                        pic_url = result.rows[0].profile_pic;
+                    }
+                    done();
+                });
+            }
+        })
+    }
 	var template_engine = req.app.settings.template_engine;
 	res.locals.session = req.session;
     res.render('main', 
-        {'checkin': checkin, 
+        {'tour_checkin': tour_checkin, 
         'first_name': first_name,
         'last_name': last_name,
         'pic_url': pic_url,
--- routes/stats.js
+++ routes/stats.js
@@ -1,9 +1,46 @@
-/*
- * GET home page.
- */
+
+console.log("MAIN");
+
+var faqs = []
+
 
 exports.show = function(req, res){
+	if (college_checkin) {
+	    pg.connect(pgconnstring, function (err, client, done) {
+	        if (err) {
+	            // error!
+	            console.log(err);
+	            done();
+	        } else {
+
+	        	var faqs_query = faq
+	        		.select(faq.question, faq.answer)
+	        		.from(faq)
+	        		.where(
+	        			faq.college_id.equals(current_college)
+	        		).toQuery();
+
+	            console.log(faqs_query);
+
+	            client.query(faqs_query, [], function (err, result) {
+	                if (err) {
+	                    // error!
+	                } else if (result.rows.length < 1) {
+	                    // no user found with that username!
+	                } else {
+	                    console.log(result.rows);
+	                    for (var row in result.rows) {
+	                    	faqs.push([row.question, row.answer]);
+	                    }
+	                }
+	                done();
+	            });
+	        }
+	    })
+	} else {
+		res.redirect('/checkin');
+	}
 	var template_engine = req.app.settings.template_engine;
 	res.locals.session = req.session;
-    res.render('stats');
+    res.render('stats', {'faqs': faqs});
 };
