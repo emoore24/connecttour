af3ef525f6519f05628ae0108b663344cb99fe6d
Carlo Biedenharn <carlo.pwn@gmail.com>
2013-05-10 12:56:11 -0400

    add done()

--- routes/stories.js
+++ routes/stories.js
@@ -71,6 +71,7 @@ exports.show = function(req, res){
                 var template_engine = req.app.settings.template_engine;
                 res.locals.session = req.session;
                 res.render('stories', { storylist: storylist, userlist: userlist});
+                done();
 	        }
 	    });
 	} else {
